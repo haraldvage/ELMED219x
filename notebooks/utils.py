287e0bdb395d01d680431cad4694b0afe851237d
Alexander Lundervold <alexander.lundervold@gmail.com>
2019-01-12 17:13:40 +0100

    minor tweak av confusion matrices

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -34,3 +34,30 @@ def plot_confusion_matrix(cm, classes, ax=None,
 
     plt.tight_layout()
     return ax
+
+def plot_confusion_matrix_with_colorbar(cm, classes, figsize=(8,8), normalize=False, 
+                                        title='Confusion matrix', 
+                                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    (This function is copied from the scikit docs.)
+    """
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar(fraction=0.046, pad=0.04)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True', fontsize=16)
+    plt.xlabel('Predicted', fontsize=16)
